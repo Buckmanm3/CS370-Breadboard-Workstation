7dd6a98476c4dc99c4ed8aacb61b5531d0b82bf0
buckmanm3 <buckmanm@colostate.edu>
2023-12-03 00:07:43 +0000

    fix Camera.py

--- Camera.py
+++ Camera.py
@@ -5,7 +5,7 @@ from time import sleep
 default = "image"
 
 def takeImage(name):
-	os.system("libcamera-jpeg -o "+name+".jpg")
+	os.system("libcamera-jpeg -o "+name+".jpg --qt-preview")
 
 def setDefault(name):
 	default = name;
